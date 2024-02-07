8840eb840515f17d5e77be3852061555f0f41a20
oktadafa <oktadafasampang@gmail.com>
2024-02-07 16:03:37 +0700

    update

---
+++ client-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ducks } from './demo'
+
+function App() {
+const [activities, setActivities] = useState([]);
+  return (
+    <>
+    <h1>Reativities</h1>
+    </> 
+  )
+}
+
+export default App
